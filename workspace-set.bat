aa3a4562fc5ca97b44836f2694598fb7c9473a7d
sworc <lightbox@nifty.com>
2021-09-04 18:26:55 +0900

    Delete workspace-set.bat

--- workspace-set.bat
+++
@@ -1,8 +0,0 @@
-mkdir .vscode
-cd .vscode
-echo { > settings.json
-echo     "code-runner.showRunIconInEditorTitleMenu": true, >> settings.json
-echo     "code-runner.runInTerminal": true, >> settings.json
-echo     "terminal.integrated.fontSize": 16, >> settings.json
-echo     "files.encoding": "shiftjis" >> settings.json
-echo } >> settings.json
